1a4557d8fb3b0216956efcdc35eebe292674a4ac
Fonserbc <fonserbc@gmail.com>
2015-01-24 05:37:25 +0800

    Remove plane case

--- Assets/Scripts/OutlineSetup.cs
+++ Assets/Scripts/OutlineSetup.cs
@@ -3,10 +3,8 @@ using System.Collections;
 
 public class OutlineSetup : MonoBehaviour {
 
-	public bool isPlane = false;
-	
 	// Use this for initialization
 	void Start () {
-		renderer.material.SetVector("_TransformScale", (isPlane? 10f : 1f) * transform.lossyScale);
+		renderer.material.SetVector("_TransformScale", transform.lossyScale);
 	}
 }
