0b7dfa311b3f148a8a7d9b3351c92bb364fd73d3
Jordi Santiago Provencio <metalzilla@gmail.com>
2015-01-25 00:12:57 +0100

    Bullets now deleted in network

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,19 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using UnityEngine;
 
 class Bullet : Photon.MonoBehaviour
 {
-
-	float bulletSpeed = 30.0f;
+	public float bulletSpeed = 30.0f;
+	public float lifeTime = 3.0f;
 
 	void Start()
 	{
 		BulletColor bcollor = GetComponent<BulletColor>();
 		bcollor.SetColor(RhythmMovement.PlayerColor(photonView.owner.ID));
 		rigidbody.velocity = transform.forward * 20.0f;
+	}
 
-		Destroy(gameObject, 3.0f);
+	void Update()
+	{
+		lifeTime -= Time.deltaTime;
+		if (lifeTime < 0.0f)
+		{
+			PhotonNetwork.Destroy(gameObject);
+		}
 	}
 }
