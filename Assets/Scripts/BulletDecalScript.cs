5552ce721acbcf480f7cc06af72a38315eaa2fee
Towerthousand <dani.torramilans@gmail.com>
2015-01-24 22:54:29 +0100

    Paredes chachis

---
+++ Assets/Scripts/BulletDecalScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletDecalScript : MonoBehaviour {
+
+	public GameObject bulletDecal;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision col) {
+		Quaternion hitRotation = Quaternion.FromToRotation(Vector3.up, col.contacts[0].normal);
+		GameObject b = (GameObject)Instantiate(bulletDecal, col.contacts[0].point, hitRotation);
+		b.transform.Rotate (new Vector3(90,0,0));
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/UpdateBeatTime.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateBeatTime : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		renderer.material.SetFloat ("_BeatTime", (float) MusicBeat.BeatTime);
+	}
+}
